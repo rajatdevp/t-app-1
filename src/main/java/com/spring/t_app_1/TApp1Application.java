e2d410eab5076bc61c827f04889878f3bfae95d9
Amar-Jeet-sahi <amarjeet.sahi70@gmail.com>
2025-05-04 16:03:48 +0530

    first commit of the proj

---
+++ src/main/java/com/spring/t_app_1/TApp1Application.java
@@ -0,0 +1,13 @@
+package com.spring.t_app_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/t_app_1/TApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.t_app_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
